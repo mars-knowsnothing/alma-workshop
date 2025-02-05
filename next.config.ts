b60af09df0dd82905139485864c8a4b5728e87a6
mars-knowsnothing <marshall.knowsnothing@gmail.com>
2025-02-05 12:17:46 +0800

    1. add export configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // 如果您的网站不是部署在域名根目录，需要设置 basePath
+  // basePath: '/your-base-path',
+  
+  // 如果您使用的是自定义域名，可能需要设置 assetPrefix
+  // assetPrefix: 'https://your-domain.com',
 };
 
 export default nextConfig;
